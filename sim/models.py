13d8331747ead52b263c6ee53c78337577c7f02f
ROMANVIKI <vikiadhi2016@gmail.com>
2024-03-11 21:43:48 +0530

    adding --requirements.txt

---
+++ sim/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+
+class Quiz(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    text = models.CharField(max_length=300)
+
+
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.CharField(max_length=300)
+    is_correct = models.BooleanField(default=False)
+
+
+
+
+    
